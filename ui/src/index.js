a9547266531c2d1ecdd559b6cdb95a17ba52964f
Blueforce Dev <blueforcedev@staturesoftware.com>
2024-12-20 18:15:25 +0200

    ui in progress

--- ui/src/index.js
+++ ui/src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.js'
 import store from './app/store'
 import { Provider } from 'react-redux'
+import * as serviceWorker from './serviceWorker';
 
 // As of React 18
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -12,4 +13,9 @@ root.render(
   <Provider store={store}>
     <App />
   </Provider>,
-)
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
